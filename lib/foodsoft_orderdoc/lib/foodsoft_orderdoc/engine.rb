44f7fa86a96c699d1bc22986686bba993fc42742
wvengen <dev-voko@willem.engen.nl>
2014-06-26 16:16:20 +0200

    protect shared supplier assets path

--- lib/foodsoft_orderdoc/lib/foodsoft_orderdoc/engine.rb
+++ lib/foodsoft_orderdoc/lib/foodsoft_orderdoc/engine.rb
@@ -1,4 +1,8 @@
 module FoodsoftOrderdoc
   class Engine < ::Rails::Engine
+    def foodsoft_default_config(cfg)
+      # protect filesystem path
+      cfg[:protected][:shared_supplier_assets_path] = true
+    end
   end
 end
