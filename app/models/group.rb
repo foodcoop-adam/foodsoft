3579bdeb6e8c69fb5053b707d2d2941d0a6d89f1
Benjamin Meichsner <benjamin.meichsner@aboutsource.net>
2014-09-21 12:47:58 +0200

    Delete memberships when deleting associated groups.
    Conflicts:
    	app/models/group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,18 +1,20 @@
-# Groups organize the User. 
+# Groups organize the User.
 # A Member gets the roles from the Group
 class Group < ActiveRecord::Base
-  has_many :memberships
+  has_many :memberships, :dependent => :destroy
   has_many :users, :through => :memberships
 
   validates :name, :presence => true, :length => {:in => 1..25}
-  
+
+  attr_reader :user_tokens
+
   scope :undeleted, -> { where(deleted_at: nil) }
 
   # Returns true if the given user if is an member of this group.
   def member?(user)
     memberships.find_by_user_id(user.id)
   end
-  
+
   # Returns all NONmembers and a checks for possible multiple Ordergroup-Memberships
   def non_members
     User.natural_order.all.reject { |u| users.include?(u) }
@@ -39,5 +41,3 @@ class Group < ActiveRecord::Base
     end
   end
 end
-
-
