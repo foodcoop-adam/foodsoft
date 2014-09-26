c4535f7c6703f6ef974e80218a87ff785b92d2e2
wvengen <dev-voko@willem.engen.nl>
2014-09-26 11:06:10 +0200

    disable migration causing problems with multishared

--- db/migrate/20140921104907_remove_stale_memberships.rb
+++ db/migrate/20140921104907_remove_stale_memberships.rb
@@ -1,5 +1,6 @@
 class RemoveStaleMemberships < ActiveRecord::Migration
   def up
-    Membership.where("group_id NOT IN (?)", Group.pluck(:id)).delete_all
+    # disable since this may delete most groups in multishared(!)
+    #Membership.where("group_id NOT IN (?)", Group.pluck(:id)).delete_all
   end
 end
