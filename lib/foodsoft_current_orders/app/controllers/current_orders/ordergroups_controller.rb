387f30c74a5034cc92a1e3a2559a971ecaf3fee4
wvengen <dev-voko@willem.engen.nl>
2014-11-17 22:55:29 +0100

    Allow to enter by pc/unit in current_orders/ordergroups too.

--- lib/foodsoft_current_orders/app/controllers/current_orders/ordergroups_controller.rb
+++ lib/foodsoft_current_orders/app/controllers/current_orders/ordergroups_controller.rb
@@ -5,15 +5,15 @@ class CurrentOrders::OrdergroupsController < ApplicationController
   before_filter :find_group_orders, only: [:index, :show]
 
   def index
-    @all_ordergroups = Ordergroup.undeleted.order(:name).all
     # sometimes need to pass id as parameter for forms
-    if @ordergroup
-      render 'show'
-    end
+    render 'show' if @ordergroup
   end
 
   def show
-    @all_ordergroups = Ordergroup.undeleted.order(:name).all
+    respond_to do |format|
+      format.html { render :show }
+      format.js   { render :show, layout: false }
+    end
   end
 
   def show_on_group_order_article_create
@@ -29,6 +29,7 @@ class CurrentOrders::OrdergroupsController < ApplicationController
   protected
 
   def find_group_orders
+    @all_ordergroups = Ordergroup.undeleted.order(:name).all
     @order_ids = Order.finished_not_closed.map(&:id)
     @ordergroup = Ordergroup.find(params[:id]) unless params[:id].nil?
     @goas = GroupOrderArticle.includes(:group_order, :order_article => [:article, :article_price]).
