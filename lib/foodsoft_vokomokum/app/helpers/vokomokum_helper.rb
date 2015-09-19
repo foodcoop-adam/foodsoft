35f96e06a6ebde74297108f6f9df999041598693
wvengen <dev-voko@willem.engen.nl>
2015-09-19 18:59:21 +0200

    Use account statement on members system for Vokomokum plugin

--- lib/foodsoft_vokomokum/app/helpers/vokomokum_helper.rb
+++ lib/foodsoft_vokomokum/app/helpers/vokomokum_helper.rb
@@ -1,7 +1,16 @@
 require 'uri'
 
 module VokomokumHelper
-  def remote_vokomokum_login_url(user=@current_user)
+  def remote_vokomokum_user_edit_url(user=@current_user)
     URI.join(FoodsoftConfig[:vokomokum_members_url], 'member/', "#{user.id.to_s}/", 'edit').to_s
+  end
+
+  def remote_vokomokum_user_url(user=@current_user)
+    URI.join(FoodsoftConfig[:vokomokum_members_url], 'member/', "#{user.id.to_s}").to_s
+  end
+
+  # Override ordergroup path with one to the members system
+  def my_ordergroup_path
+    "#{remote_vokomokum_user_url}#transactions"
   end
 end
