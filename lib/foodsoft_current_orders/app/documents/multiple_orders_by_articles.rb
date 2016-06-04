3cbd909eec4233cc60db153c69c5e71020bb2e83
wvengen <dev-voko@willem.engen.nl>
2016-06-04 22:13:59 +0200

    Fix current_order's order_articles preloading

--- lib/foodsoft_current_orders/app/documents/multiple_orders_by_articles.rb
+++ lib/foodsoft_current_orders/app/documents/multiple_orders_by_articles.rb
@@ -2,44 +2,47 @@
 class MultipleOrdersByArticles < OrderPdf
   include OrdersHelper
 
+  # optimal value depends on the average number of ordergroups ordering an article
+  #   as well as the available memory
+  BATCH_SIZE = 50
+
+  attr_reader :order
+
   def filename
-    I18n.t('documents.multiple_orders_by_articles.filename', count: @order.count) + '.pdf'
+    I18n.t('documents.multiple_orders_by_articles.filename', count: order.count) + '.pdf'
   end
 
   def title
-    I18n.t('documents.multiple_orders_by_articles.title', count: @order.count)
-  end
-
-  def order_articles
-    @order_articles ||= OrderArticle.joins(:order, :article).where(:orders => {:id => @order}).reorder('orders.id, articles.name')
+    I18n.t('documents.multiple_orders_by_articles.title', count: order.count)
   end
 
   def body
-    order_articles.each do |order_article|
+    each_order_article do |order_article|
       down_or_page
 
       rows = []
       dimrows = []
       has_units_str = ''
-      for goa in order_article.group_order_articles.natural_order
+      sum_result = 0
+      each_group_order_article_for(order_article) do |goa|
         units = result_in_units(goa, order_article.article)
-        rows << [show_group(goa.group_order.ordergroup),
+        rows << [goa.group_order.ordergroup.name,
                  goa.tolerance > 0 ? "#{goa.quantity} + #{goa.tolerance}" : goa.quantity,
                  goa.result,
                  units,
                  number_to_currency(goa.total_price(order_article))]
         dimrows << rows.length if goa.result == 0
         has_units_str = units.to_s if units.to_s.length > has_units_str.length # hack for prawn line-breaking units cell
+        sum_result += goa.result
       end
       next if rows.length == 0
-      sum = order_article.group_orders_sum
       rows.unshift I18n.t('documents.order_by_articles.rows').dup # table header
       rows[0][2] = {content: rows[0][2], colspan: 2}
 
       rows << [I18n.t('documents.order_by_groups.sum'),
                order_article.tolerance > 0 ? "#{order_article.quantity} + #{order_article.tolerance}" : order_article.quantity,
-               sum[:quantity],
-               result_in_units(sum[:quantity], order_article.article),
+               sum_result,
+               result_in_units(sum_result, order_article.article),
                nil] #number_to_currency(sum[:price])]
 
       text "<b>#{order_article.article.name}</b> " +
@@ -74,6 +77,28 @@ class MultipleOrdersByArticles < OrderPdf
 
   def pdf_add_page_breaks?
     super 'order_by_articles'
+  end
+
+  def order_articles
+    OrderArticle.where(order_id: order).ordered.
+      includes(:article).
+      reorder('order_articles.order_id, articles.name').
+      preload(:article_price). # preload not join, just in case it went missing
+      preload(:order => :supplier, :group_order_articles => {:group_order => :ordergroup})
+  end
+
+  def each_order_article
+    order_articles.find_each_with_order(batch_size: BATCH_SIZE) {|oa| yield oa }
+  end
+
+  def group_order_articles_for(order_article)
+    goas = order_article.group_order_articles.to_a
+    goas.sort_by! {|goa| goa.group_order.ordergroup.name }
+    goas
+  end
+
+  def each_group_order_article_for(group_order)
+    group_order_articles_for(group_order).each {|goa| yield goa }
   end
 
 end
