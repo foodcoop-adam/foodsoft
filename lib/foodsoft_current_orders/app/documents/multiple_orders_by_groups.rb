1ab3607bc7bd6a336b351e8d37f188f4b874f02d
wvengen <dev-voko@willem.engen.nl>
2016-06-09 19:43:13 +0200

    Fix current_order's ordergroups document preloading

--- lib/foodsoft_current_orders/app/documents/multiple_orders_by_groups.rb
+++ lib/foodsoft_current_orders/app/documents/multiple_orders_by_groups.rb
@@ -2,35 +2,33 @@
 class MultipleOrdersByGroups < OrderPdf
   include OrdersHelper
 
+  # optimal value depends on the number of articles ordered on average by each
+  #   ordergroup as well as the available memory
+  BATCH_SIZE = 50
+
+  attr_reader :order
+
   def filename
-    I18n.t('documents.multiple_orders_by_groups.filename', count: @order.count) + '.pdf'
+    I18n.t('documents.multiple_orders_by_groups.filename', count: order.count) + '.pdf'
   end
 
   def title
-    I18n.t('documents.multiple_orders_by_groups.title', count: @order.count)
-  end
-
-  def ordergroups
-    unless @ordergroups
-      @ordergroups = Ordergroup.joins(:orders).where(orders: {id: @order}).select('distinct(groups.id)').select('groups.*').natural_order
-      @ordergroups = @ordergroups.where(id: @options[:ordergroup]) if @options[:ordergroup]
-    end
-    @ordergroups
+    I18n.t('documents.multiple_orders_by_groups.title', count: order.count)
   end
 
   def body
     # Start rendering
-    ordergroups.each do |ordergroup|
+    each_ordergroup do |ordergroup|
       down_or_page 15
 
       totals = {net_price: 0, deposit: 0, gross_price: 0, fc_price: 0, fc_markup_price: 0}
       taxes = Hash.new {0}
       rows = []
       dimrows = []
-      group_order_articles = GroupOrderArticle.joins(:group_order => :order).where(:group_orders =>{:ordergroup_id => ordergroup.id}).where(:orders => {id: @order}).includes(:order_article => :article_price).reorder('orders.id').natural_order
-      has_tolerance = group_order_articles.where('article_prices.unit_quantity > 1').any?
+      has_tolerance = false
 
-      group_order_articles.each do |goa|
+      each_group_order_article_for(ordergroup) do |goa|
+        has_tolerance = true if goa.order_article.price.unit_quantity > 1
         price = goa.order_article.price
         goa_totals = goa.total_prices
         totals[:net_price] += goa_totals[:net_price]
@@ -47,7 +45,7 @@ class MultipleOrdersByGroups < OrderPdf
                   goa.result,
                   result_in_units(goa),
                   number_to_currency(goa_totals[:price]),
-                  (goa.order_article.price.unit_quantity if has_tolerance)]
+                  goa.order_article.price.unit_quantity]
         dimrows << rows.length if goa.result == 0
       end
       next if rows.length == 0
@@ -62,17 +60,16 @@ class MultipleOrdersByGroups < OrderPdf
         price_details << "#{Article.human_attribute_name :tax} #{number_to_percentage tax} #{number_to_currency tax_price}" if tax_price > 0
       end
       price_details << "#{Article.human_attribute_name :fc_share_short} #{number_to_percentage ordergroup.markup_pct} #{number_to_currency totals[:fc_markup_price]}"
-      rows << [{content: ('  ' + price_details.join('; ') if totals[:fc_price] > 0), colspan: 8}]
+      rows << [{content: ('  ' + price_details.join('; ') if totals[:fc_price] > 0), colspan: 8}, nil]
 
       # table header
       rows.unshift I18n.t('documents.order_by_groups.rows').dup
       rows.first.insert(1, Article.human_attribute_name(:supplier))
       rows.first[5] = {content: rows.first[5], colspan: 2}
-      if has_tolerance
-        rows.first[-1] = {image: "#{Rails.root}/app/assets/images/package-bg.png", scale: 0.6, position: :center}
-      else
-        rows.first[-1] = nil
-      end
+      rows.first[-1] = {image: "#{Rails.root}/app/assets/images/package-bg.png", scale: 0.6, position: :center}
+
+      # last column showing unit_quantity is useless if they're all one
+      rows.each {|row| row[-1] = nil} unless has_tolerance
 
       text show_group(ordergroup), size: fontsize(13), style: :bold
       table rows, width: bounds.width, cell_style: {size: fontsize(8), overflow: :shrink_to_fit} do |table|
@@ -92,7 +89,7 @@ class MultipleOrdersByGroups < OrderPdf
         table.row(rows.length-1).text_color = '999999'
         table.row(rows.length-1).size = fontsize(7)
         table.row(rows.length-1).padding = [0, 5, 0, 5]
-        table.row(rows.length-1).height = 0 if totals[:fc_price] == 0
+        table.row(rows.length-1).height = 0 unless totals[:fc_price] > 0
 
         table.column(0).width = 150 # @todo would like to set minimum width here
         table.column(1).width = 62
@@ -116,10 +113,41 @@ class MultipleOrdersByGroups < OrderPdf
     end
   end
 
-  protected
+  private
 
   def pdf_add_page_breaks?
     super 'order_by_groups'
+  end
+
+  def ordergroups
+    s = Ordergroup.
+          joins(:group_orders).
+          where(group_orders: {order_id: order}).
+          group('groups.id').
+          reorder(:name)
+    s = s.where(id: @options[:ordergroup]) if @options[:ordergroup]
+    s
+  end
+
+  def each_ordergroup
+    ordergroups.find_in_batches_with_order(batch_size: BATCH_SIZE) do |ordergroups|
+      @group_order_article_batch = GroupOrderArticle.
+        joins(:group_order).
+        where(group_orders: {order_id: order}).
+        where(group_orders: {ordergroup_id: ordergroups.map(&:id)}).
+        order('group_orders.order_id, group_order_articles.id').
+        preload(group_order: {order: :supplier}).
+        preload(order_article: [:article, :article_price, :order])
+      ordergroups.each {|ordergroup| yield ordergroup }
+    end
+  end
+
+  def group_order_articles_for(ordergroup)
+    @group_order_article_batch.select {|goa| goa.group_order.ordergroup_id == ordergroup.id }
+  end
+
+  def each_group_order_article_for(ordergroup)
+    group_order_articles_for(ordergroup).each {|goa| yield goa }
   end
 
 end
